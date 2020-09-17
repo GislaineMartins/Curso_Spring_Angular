33cedf354088ba060ce9e94b8b8521c00636de46
Gislaine <gimcjf@gmail.com>
2020-09-17 14:05:12 -0300

    primeira aula do curso

---
+++ typeScsript/aula01/inference.js
@@ -0,0 +1,9 @@
+function showLog(messege) {
+    console.log(messege);
+}
+showLog("Ola");
+var test = ["Hello", "Test"];
+var isEnable = true;
+//let testDois: [string] = ['Hello', 'Test'];
+console.log(test[0]);
+console.log(test[1]);
---
+++ typeScsript/aula01/inference.ts
@@ -0,0 +1,12 @@
+function showLog(messege: string){
+    console.log(messege);
+}
+
+showLog("Ola");
+
+let test: Array<string> = ["Hello", "Test"];
+let isEnable: boolean = true;
+//let testDois: [string] = ['Hello', 'Test'];
+console.log(test[0])
+console.log(test[1])
+
